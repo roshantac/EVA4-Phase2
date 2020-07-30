a3cb9e5513096081d8907573bdb77f706787791d
sujitojha1 <payalchakraborty@Payals-MacBook-Air.local>
2020-07-31 00:58:57 +0530

    class list updated

--- Modules/data_loader.py
+++ Modules/data_loader.py
@@ -17,7 +17,7 @@ class DroneDataset(Dataset):
             data_file = open('testData.csv','r')
         
         self.data = list(csv.reader(data_file))
-        self.classes = ["Flying Birds", "Large QuadCopters", "Small QuadCopters", "Winged Drones"]
+        self.classes = ['Large QuadCopters', 'Flying Birds', 'Small QuadCopters', 'Winged Drones']
 
     def __len__(self):
         return len(self.data)
@@ -26,7 +26,7 @@ class DroneDataset(Dataset):
         imgLoc, target =self.data[idx][0], int(self.data[idx][2])
         image = np.array(Image.open(imgLoc))
         print(f"idx:{idx}")
-        print(f"target :{self.classes[target]}")
+        print(f"target :{imgLoc}")
         print(f"imgLoc :{self.classes[target]}")
 
         if (len(image.shape) == 2) or (len(image.shape)==3 and image.shape[-1]==1):
