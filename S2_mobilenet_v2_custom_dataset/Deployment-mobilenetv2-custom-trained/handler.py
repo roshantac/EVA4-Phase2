9de095399a029198a660c9936963757602bc1fc8
sujitojha1 <payalchakraborty@Payals-MacBook-Air.local>
2020-08-06 02:09:55 +0530

    updated mean and std in transformation for deployment code

--- S2_mobilenet_v2_custom_dataset/Deployment-mobilenetv2-custom-trained/handler.py
+++ S2_mobilenet_v2_custom_dataset/Deployment-mobilenetv2-custom-trained/handler.py
@@ -61,8 +61,8 @@ def transform_image(image_bytes):
             transforms.Resize(255),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
+            transforms.Normalize(mean=[0.5404, 0.5918, 0.6219],
+                                 std=[0.2771, 0.2576, 0.2998])
         ])
         image = Image.open(io.BytesIO(image_bytes))
         return transformations(image).unsqueeze(0)
