718ddfa0a663148bfd8657d3f452dd6bf8e7075e
seenu-g <srinivasan.gsvasan@gmail.com>
2020-08-07 01:28:39 +0530

    updates

--- Modules/find_mean_stddev.py
+++ Modules/find_mean_stddev.py
@@ -15,10 +15,10 @@ def find_mean_standard_deviation(image_dir) :
 
     for sub_dir in image_folders :
         temp = image_dir + sub_dir
-        current_dir = next(os.walk(temp))[2]
-        file_count = 0
+        #current_dir = next(os.walk(temp))[2]
+        #file_count = 0
         for image_name in os.listdir(temp):
-            file_count = file_count + 1
+            #file_count = file_count + 1
             if image_name.endswith(".jpg"): 
                 img = Image.open(temp +"/" +image_name)
                 x = np.array(img)/255
@@ -26,7 +26,7 @@ def find_mean_standard_deviation(image_dir) :
                 sq += np.sum(np.square(x), axis=(0,1))
                 n += x.shape[0]*x.shape[1]
             continue
-        print(len(current_dir), file_count)
+        #print(len(current_dir), file_count)
     mean = s/n
     std_deviation = np.sqrt((sq/n - np.square(mean)))
     print(mean, sq/n, std_deviation, n)
