1a7a2bda54eaba3975cc1978dd87d0430ace234e
Muhammed Roshan <roshantac@gmail.com>
2020-08-08 00:27:51 +0530

    Update utils.py

--- Modules/utils.py
+++ Modules/utils.py
@@ -65,7 +65,9 @@ def ShowMissclassifiedImages(model, dataloaders, class_names, class_id, device,d
                   imagex = inputs.cpu().data[j]
                   imagex = np.transpose(imagex, (1, 2, 0))
                   imagex=imagex.numpy()
-                  imagex = imagex/np.amax(imagex)
+                  mean = np.array([0.53713346, 0.58979464, 0.62127595])
+                  std = np.array([0.27420551, 0.25534403, 0.29759673])
+                  imagex = std*imagex  + mean
                   imagex = np.clip(imagex, 0, 1)       
                   axs[row,col].imshow(imagex) 
                   fig.tight_layout(pad=2.0)
